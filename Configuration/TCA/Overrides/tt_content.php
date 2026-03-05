dc5b1e9459f7b9f5a8de9d7987293675c537a214
Marek Skopal <skopal.marek@gmail.com>
2026-03-05 22:08:12 +0100

    Fix tca

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,8 +2,20 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die;
 
-ExtensionUtility::registerPlugin('MsPricing', 'Pricing', 'Pricing Table');
+$pluginSignature = ExtensionUtility::registerPlugin(
+    'MsPricing',
+    'Pricing',
+    'Pricing Table',
+);
+
+ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin, pages, recursive',
+    $pluginSignature,
+    'after:palette:headers'
+);
